05d234dfc7122069d810acb661d5146db3ea4be1
Suman <rai12345suman@gmail.com>
2019-01-26 17:43:11 +0800

    edit script

--- runme.sh
+++ runme.sh
@@ -1,12 +1,12 @@
   #!/bin/bash
  
- echo "Install Python 3.7 and required dependencies"
- apt-get update
- apt-get -y upgrade
- apt-get install -y python3.7
- apt install -y python3-pip
- pip3 install -r requirements.txt --user 
+echo "Install Python 3.7 and required dependencies"
+sudo apt-get update
+sudo apt-get -y upgrade
+sudo apt-get install -y python3.7
+sudo apt install -y python3-pip
+sudo pip3 install -r requirements.txt --user 
  
- echo "Start Python Application"
- python3 manage.py runserver localhost:80
+echo "Start Python Application"
+sudo python3 manage.py runserver localhost:80
  
