f86783e050b6814d891d5851ebbc3f9f888687ef
Suman <rai12345suman@gmail.com>
2019-01-27 10:59:16 +0800

    pip3 install sklearn

--- runme.sh
+++ runme.sh
@@ -8,6 +8,7 @@ sudo apt install -y python3-pip
 sudo pip3 install -r requirements.txt --user 
  
 echo "Start Python Application"
+sudo pip3 install sklearn --user
 sudo python3 -m nltk.downloader all
 sudo python3 manage.py runserver 0.0.0.0:80
  
